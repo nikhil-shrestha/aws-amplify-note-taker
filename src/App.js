d4112b3db059af56c70a428383fb3df0c03035a9
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-16 09:10:41 +0545

    Updating Notes with updateNote mutation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
 
-import { createNote, deleteNote } from './graphql/mutations';
+import { createNote, deleteNote, updateNote } from './graphql/mutations';
 import { listNotes } from './graphql/queries';
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [noteId, setNoteId] = useState('');
   const [note, setNote] = useState('');
 
   useEffect(() => {
@@ -19,9 +20,24 @@ function App() {
 
   const handleChange = (event) => setNote(event.target.value);
 
+  const hasExistingNote = () => {
+    if (noteId) {
+      // is the id a valid id
+      const isNote = notes.findIndex((note) => note.id === noteId) > -1;
+      return isNote;
+    }
+    return false;
+  };
+
   const handleAddNote = async (event) => {
     event.preventDefault();
 
+    // check if we have an existing note, if so update it
+    if (hasExistingNote()) {
+      console.log('note updated!');
+      handleUpdateNote();
+      return;
+    }
     const input = {
       note,
     };
@@ -37,6 +53,26 @@ function App() {
     setNotes([newNote, ...notes]);
   };
 
+  const handleUpdateNote = async () => {
+    const input = { id: noteId, note };
+    const { data } = await API.graphql(
+      graphqlOperation(updateNote, {
+        input,
+      })
+    );
+
+    const updatedNote = data.updateNote;
+    setNote('');
+    setNoteId('');
+    const index = notes.findIndex((note) => note.id === updatedNote.id);
+    const updatedNotes = [
+      ...notes.slice(0, index),
+      updatedNote,
+      ...notes.slice(index + 1),
+    ];
+    setNotes(updatedNotes);
+  };
+
   const handleDeleteNote = async (noteId) => {
     const input = { id: noteId };
 
@@ -49,6 +85,11 @@ function App() {
     const deletedNoteId = data.deleteNote.id;
     const updatedNotes = notes.filter((note) => note.id !== deletedNoteId);
     setNotes(updatedNotes);
+  };
+
+  const handleSetNote = ({ note, id }) => {
+    setNote(note);
+    setNoteId(id);
   };
 
   return (
@@ -66,14 +107,16 @@ function App() {
           value={note}
         />
         <button type="submit" className="pa2 f4">
-          Add Note
+          {noteId ? 'Update Note' : 'Add Note'}
         </button>
       </form>
 
       {/* Notes List */}
       {notes.map((item) => (
         <div key={item.id} className="flex items-center">
-          <li className="list pa1 f3">{item.note}</li>
+          <li onClick={() => handleSetNote(item)} className="list pa1 f3">
+            {item.note}
+          </li>
           <button
             className="bg-transparent bn f4"
             onClick={() => handleDeleteNote(item.id)}
